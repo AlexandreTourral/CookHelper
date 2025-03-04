9c7cd3b05070cde643cdc743d29b014274a44572
Alexandre Tourral <alexandretourral@MacBook-Pro-de-Alexandre.local>
2025-03-04 09:14:19 +0100

    feat: new Navbar

--- src/ui/components/Layout.tsx
+++ src/ui/components/Layout.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { NavBar } from "../organisms";
 
 export function Layout() {
   return (
     <div>
-      <div>
-        NavBar
-      </div>
+      <NavBar />
       <Outlet />
     </div>
   )
